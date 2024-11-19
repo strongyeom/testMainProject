f1fc1f2cffd43720fecd3a833a056ea2c18255f5
yeomsungpil <present7256@gmail.com>
2024-11-19 23:33:30 +0900

    feat: 초기 프로젝트 생성

---
+++ TestMainProject/TestMainProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestMainProject
+//
+//  Created by 염성필 on 11/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
